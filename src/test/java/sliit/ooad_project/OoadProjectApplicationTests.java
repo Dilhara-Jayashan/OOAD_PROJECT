1f7298c4e4711f14237aeedfdad557ebe304f1d1
Dilhara-Jayashan <140555849+Dilhara-Jayashan@users.noreply.github.com>
2024-11-25 15:06:30 +0530

    create again

---
+++ src/test/java/sliit/ooad_project/OoadProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sliit.ooad_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OoadProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
